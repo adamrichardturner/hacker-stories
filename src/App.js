7079cf197a124b68c0057b7772c95193149f8b5f
Adam Turner <hello@adamrichardturner.dev>
2021-07-13 19:24:47 +0100

    Practised with the React.useState hook

--- src/App.js
+++ src/App.js
@@ -23,13 +23,19 @@ const Item = (props) => {
 }
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
   const handleChange = (event) => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   }
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange}/>
+
+      <p>
+        Search for <strong>{searchTerm}</strong>.
+      </p>
+
     </div>
   )
 }
