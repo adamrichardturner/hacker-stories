f19bb60f7c926b829c2fd87996afe7e47f00237a
Adam Turner <hello@adamrichardturner.dev>
2021-07-14 23:02:21 +0100

    Worked with making custom hooks

--- src/App.js
+++ src/App.js
@@ -1,64 +1,26 @@
 import * as React from 'react';
 
-// Component that takes props as argument 
-// using map it renders the Item component
-// HTML with props arg (stories) each object
-// property displayed.
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
 
-const List = ({list}) => {
-  return(
-    <ul>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
-  )
-}
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-// Item takes props as argument (in this case stories) 
-// and returns the url, title, author, comments and points
-// of each object prop values in stories.
-
-const Item = ({item}) => {
-  return(
-    <li>
-    <span>
-      <a href={item.url}>{item.title}</a> </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-  </li>
-  )
-}
-
-// Search takes props as arg onSearch={handleSearch}
-// Logging the search term beneath the input field
-
-const Search = ({ search, onSearch }) => {
-  // Props is arg which in this case is an event handler onSearch
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input 
-      id="search" 
-      type="text" 
-      onChange={onSearch}
-      value={search}    
-      />
-
-    </div>
-  )
-}
+  return [value, setValue];
+};
 
 const App = () => {
   const stories = [
     {
       title: 'React',
-      url: 'httpsL//reactjs.org/',
+      url: 'https://reactjs.org/',
       author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
-      objectID: 0
+      objectID: 0,
     },
     {
       title: 'Redux',
@@ -69,16 +31,12 @@ const App = () => {
       objectID: 1,
     },
   ];
-  // Below const uses React.useState to set SearchTerm to initial state
-  // "" and second arg is a function which updates the state setSearchTerm
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || 'React'
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
   );
 
-  React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
-  
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -86,6 +44,7 @@ const App = () => {
   const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -95,9 +54,39 @@ const App = () => {
       <hr />
 
       <List list={searchedStories} />
-
     </div>
   );
-}
+};
+
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
+  </div>
+);
+
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
+);
 
 export default App;
