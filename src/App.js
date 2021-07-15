659f31f19ad433d416d15c037cb38fc96095d061
Adam Turner <hello@adamrichardturner.dev>
2021-07-15 20:45:14 +0100

    Learnt about handlers, callback handlers, and inline handlers

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const useSemiPersistentState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -36,6 +36,16 @@ const App = () => {
     'search',
     'React'
   );
+
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -62,7 +72,10 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} 
+            onRemoveItem={handleRemoveStory}
+            />
+
     </div>
   );
 };
@@ -99,15 +112,24 @@ return (
   </>
 );
 };
-const List = ({ list }) => (
+
+const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item 
+      key={item.objectID} 
+      item={item} 
+      onRemoveItem={onRemoveItem}
+      />
     ))}
   </ul>
 );
 
-const Item = ({ item }) => (
+const Item = ({ item, onRemoveItem }) => {
+  const handleRemoveItem = () => {
+    onRemoveItem(item);
+  }
+  return(
   <li>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -115,7 +137,13 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={handleRemoveItem.bind(null, item)}>
+        Dismiss
+      </button>
+    </span>
   </li>
-);
+  );
+};
 
 export default App;
